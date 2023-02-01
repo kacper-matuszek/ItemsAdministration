7e41217d8065da582a35cdf1719d551726cf3ba2
kacper-matuszek <kacpermatuszek2@gmail.com>
2023-02-01 19:40:27 +0100

    - Add auditable.

--- common/Domain/Models/BaseAggregate.cs
+++ common/Domain/Models/BaseAggregate.cs
@@ -1,8 +1,9 @@
-﻿using ItemsAdministration.Common.Domain.Models.Interfaces;
+﻿using System;
+using ItemsAdministration.Common.Domain.Models.Interfaces;
 
 namespace ItemsAdministration.Common.Domain.Models;
 
-public abstract class BaseAggregate<T> : IIdentifiable<T>, IPersistable, IVersionable
+public abstract class BaseAggregate<T> : IIdentifiable<T>, IPersistable, IVersionable, IAuditable
     where T : notnull
 {
     private bool _versionIncremented;
@@ -10,12 +11,19 @@ public abstract class BaseAggregate<T> : IIdentifiable<T>, IPersistable, IVersio
     public T Id { get; }
     public long Version { get; private set; } = 1;
 
-    protected BaseAggregate(T id) => 
+    public DateTime CreatedAt { get; }
+    public DateTime? UpdatedAt { get; private set; }
+
+    protected BaseAggregate(T id)
+    {
         Id = id;
+        CreatedAt = DateTime.UtcNow;
+    }
 
     public void OnPersist()
     {
         IncrementVersion();
+        UpdatedAt = DateTime.UtcNow;
     }
 
     private void IncrementVersion()
---
+++ common/Domain/Models/Interfaces/IAuditable.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ItemsAdministration.Common.Domain.Models.Interfaces;
+
+internal interface IAuditable
+{
+    DateTime CreatedAt { get; }
+    DateTime? UpdatedAt { get; }
+}
