a3ec5e54707ec7ebee3d6ef6b1204b605d1da10a
kacper-matuszek <kacpermatuszek2@gmail.com>
2023-02-01 20:34:05 +0100

    - Add base guid aggregate repository.

---
+++ common/Infrastructure.EF/Repositories/BaseGuidAggregateRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ItemsAdministration.Common.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItemsAdministration.Common.Infrastructure.EF.Repositories;
+
+public abstract class BaseGuidAggregateRepository<TAggregate, TContext> : BaseIdentifiableRepository<TAggregate, Guid, TContext>
+    where TAggregate : BaseGuidAggregate
+    where TContext : DbContext
+{
+    protected BaseGuidAggregateRepository(TContext context)
+        : base(context) { }
+}
