e85362c9b91e891f54dfbd19b7eabbf0c0f5e6a8
kacper-matuszek <kacpermatuszek2@gmail.com>
2023-02-01 20:06:49 +0100

    - Add base guid aggregate configuration.

---
+++ common/Infrastructure.EF/Configurations/BaseGuidAggregateConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using ItemsAdministration.Common.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ItemsAdministration.Common.Infrastructure.EF.Configurations;
+
+public abstract class BaseGuidAggregateConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+    where TEntity : BaseGuidAggregate
+{
+    public void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Version)
+            .IsRequired()
+            .IsConcurrencyToken();
+    }
+}
