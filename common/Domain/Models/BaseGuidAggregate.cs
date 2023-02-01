b253eba65178aa48b3b2b20145ea0afc7e084607
kacper-matuszek <kacpermatuszek2@gmail.com>
2023-02-01 19:28:54 +0100

    - Add base guid aggregate.

---
+++ common/Domain/Models/BaseGuidAggregate.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ItemsAdministration.Common.Domain.Models;
+
+public abstract class BaseGuidAggregate : BaseAggregate<Guid>
+{
+    protected BaseGuidAggregate(Guid id)
+        : base(id) { }
+}
