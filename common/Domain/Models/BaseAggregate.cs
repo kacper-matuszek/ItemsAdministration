747448bf6673adea87a831f1edc064fb8792b231
kacper-matuszek <kacpermatuszek2@gmail.com>
2023-02-01 19:27:43 +0100

    - Add base aggregate.

---
+++ common/Domain/Models/BaseAggregate.cs
@@ -0,0 +1,31 @@
+﻿using ItemsAdministration.Common.Domain.Models.Interfaces;
+
+namespace ItemsAdministration.Common.Domain.Models;
+
+public abstract class BaseAggregate<T> : IIdentifiable<T>, IPersistable, IVersionable
+    where T : notnull
+{
+    private bool _versionIncremented;
+
+    public T Id { get; }
+    public long Version { get; private set; } = 1;
+
+    protected BaseAggregate(T id) => 
+        Id = id;
+
+    public void OnPersist()
+    {
+        IncrementVersion();
+    }
+
+    private void IncrementVersion()
+    {
+        if (_versionIncremented)
+        {
+            return;
+        }
+
+        Version++;
+        _versionIncremented = true;
+    }
+}
