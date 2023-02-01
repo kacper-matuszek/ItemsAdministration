55737e2969322e8fb3d244828b910f1184c70ee6
kacper-matuszek <kacpermatuszek2@gmail.com>
2023-02-01 19:56:06 +0100

    - Add base db context.

---
+++ common/Infrastructure.EF/BaseDbContext.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ItemsAdministration.Common.Domain.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItemsAdministration.Common.Infrastructure.EF;
+
+public abstract class BaseDbContext : DbContext
+{
+    protected BaseDbContext(DbContextOptions options)
+        : base(options)
+    { }
+
+    public override int SaveChanges()
+    {
+        SaveEntities();
+        return base.SaveChanges();
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        SaveEntities();
+        return await base.SaveChangesAsync(cancellationToken);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+
+    private void SaveEntities()
+    {
+        ChangeTracker.DetectChanges();
+        var modifiedEntities = ChangeTracker.Entries()
+            .Where(e => e.State == EntityState.Modified)
+            .ToArray();
+
+        foreach (var modifiedEntity in modifiedEntities)
+        {
+            (modifiedEntity as IPersistable)?.OnPersist();
+        }
+    }
+}
