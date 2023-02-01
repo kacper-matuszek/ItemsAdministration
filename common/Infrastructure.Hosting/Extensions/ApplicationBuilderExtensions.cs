430a58fb82e8b892904666583dd1c1abbe3e61e1
kacper-matuszek <kacpermatuszek2@gmail.com>
2023-02-01 21:23:51 +0100

    - Add use database auto migration.

---
+++ common/Infrastructure.Hosting/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ItemsAdministration.Common.Infrastructure.Hosting.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseDatabaseAutoMigration<TEfContext>(this IApplicationBuilder applicationBuilder)
+        where TEfContext : DbContext
+    {
+        using var scope = applicationBuilder.ApplicationServices.CreateScope();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<TEfContext>>();
+        try
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<TEfContext>();
+            logger.LogInformation("Running DbContext {0} migrations...", typeof(TEfContext).Name);
+            dbContext.Database.Migrate();
+        }
+        catch (Exception e)
+        {
+            logger.LogError(exception: e, message: "Error occured during running migrations.");
+        }
+
+        return applicationBuilder;
+    }
+}
