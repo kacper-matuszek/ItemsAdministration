011f6cde6513f142ab0f8c6acfe863619356744a
Kacper096 <kacpermatuszek2@gmail.com>
2023-02-03 14:18:45 +0100

    Add base validator. Add validation result exception.

---
+++ common/Shared/Validators/BaseValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using ItemsAdministration.Common.Shared.Exceptions;
+using System;
+using System.Linq;
+
+namespace ItemsAdministration.Common.Shared.Validators;
+
+public abstract class BaseValidator<T> : AbstractValidator<T>
+{
+    protected const string PredicateValidatorCode = "PredicateValidator";
+
+    protected bool IsValid(T candidate, out ValidationResultException[] exceptions)
+    {
+        exceptions = Array.Empty<ValidationResultException>();
+        var validationResult = Validate(candidate);
+
+        if (validationResult.IsValid)
+        {
+            return true;
+        }
+
+        exceptions = validationResult.Errors
+            .Where(e => !e.ErrorCode.Equals(PredicateValidatorCode))
+            .Select(e => new ValidationResultException(e.ErrorCode, e.CustomState))
+            .ToArray();
+
+        return false;
+    }
+}
