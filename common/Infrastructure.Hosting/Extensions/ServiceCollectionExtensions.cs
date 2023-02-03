a031ad1d3c174fb26db04fd33cdc81f781233d08
Kacper096 <kacpermatuszek2@gmail.com>
2023-02-03 14:04:33 +0100

    Add ItemsAdministration.Common.Shared

--- common/Infrastructure.Hosting/Extensions/ServiceCollectionExtensions.cs
+++ common/Infrastructure.Hosting/Extensions/ServiceCollectionExtensions.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MediatR;
+using ItemsAdministration.Common.Shared.Extensions;
 
 namespace ItemsAdministration.Common.Infrastructure.Hosting.Extensions;
 
--- common/Shared/Extensions/TypExtensions.cs
+++ common/Shared/Extensions/TypExtensions.cs
@@ -2,16 +2,16 @@
 using System.Linq;
 using System.Reflection;
 
-namespace ItemsAdministration.Common.Infrastructure.Hosting.Extensions;
+namespace ItemsAdministration.Common.Shared.Extensions;
 
-public static class TypeExtensions
+public static class TypExtensions
 {
     public static Type[] GetTypesBasedOnAttribute<TAttribute>(this Assembly assembly)
-        where TAttribute : Attribute =>
-        assembly
-            .GetTypes()
-            .Where(t => t.IsDefined(typeof(TAttribute), inherit: false))
-            .ToArray();
+    where TAttribute : Attribute =>
+    assembly
+        .GetTypes()
+        .Where(t => t.IsDefined(typeof(TAttribute), inherit: false))
+        .ToArray();
 
     public static (Type DerivedType, Type InterfaceType)[] GetDerivedTypesWithInterfaces<TAttribute>(this Assembly assembly)
         where TAttribute : Attribute =>
