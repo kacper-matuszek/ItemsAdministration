165943f6fbb4953172674910fe952d7a64865d7b
Kacper096 <kacpermatuszek2@gmail.com>
2023-02-03 13:17:43 +0100

    - Add dispatcher interface

---
+++ common/Application/Interfaces/Dispatchers/IDispatcher.cs
@@ -0,0 +1,12 @@
+﻿using ItemsAdministration.Common.Application.Interfaces.Commands;
+using ItemsAdministration.Common.Application.Interfaces.Queries;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ItemsAdministration.Common.Application.Interfaces.Dispatchers;
+
+public interface IDispatcher
+{
+    Task Send(ICommand command, CancellationToken cancellationToken = default);
+    Task<TResult> Query<TResult>(IQuery<TResult> query, CancellationToken cancellationToken = default);
+}
