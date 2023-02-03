7ac63aef9e17dbec9e4f0d6341879197b4bad92f
Kacper096 <kacpermatuszek2@gmail.com>
2023-02-03 16:56:41 +0100

    Add configuration extensions.

---
+++ common/Infrastructure.Hosting/Extensions/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ItemsAdministration.Common.Infrastructure.Hosting.Extensions;
+
+public static class ConfigurationBuilderExtensions
+{
+    private const string ConfigurationDir = "Configurations";
+
+    public static IConfigurationBuilder AddConfiguration(this IConfigurationBuilder builder)
+    {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        builder.AddJsonFile($"./{ConfigurationDir}/appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"./{ConfigurationDir}/appsettings.{environment}.json", optional: true);
+
+        return builder;
+    }
+}
