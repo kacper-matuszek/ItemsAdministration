714ed5c1717ef7678324f619e479cb7ddfe7bd60
Kacper096 <kacpermatuszek2@gmail.com>
2023-02-03 15:03:37 +0100

    Add localization service

---
+++ common/Infrastructure.Hosting/Localizations/Interfaces/ILocalizationService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace ItemsAdministration.Common.Infrastructure.Hosting.Localizations.Interfaces;
+
+public interface ILocalizationService
+{
+    LocalizedString GetLocalizedString(string key);
+    LocalizedString GetLocalizedString(string key, string culture);
+    LocalizedString GetDefaultLocalizedString(string key);
+}
---
+++ common/Infrastructure.Hosting/Localizations/LocalizationService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Globalization;
+
+namespace ItemsAdministration.Common.Infrastructure.Hosting.Localizations;
+
+internal sealed class LocalizationService
+{
+    private const string DefaultCulture = "en-US";
+
+    private readonly IStringLocalizer _localizer;
+
+    public LocalizationService(IStringLocalizer localizer)
+    {
+        _localizer = localizer;
+    }
+
+    public LocalizedString GetLocalizedString(string key) =>
+        _localizer.GetString(key);
+
+    public LocalizedString GetLocalizedString(string key, string culture)
+    {
+        if (string.IsNullOrWhiteSpace(culture))
+            culture = DefaultCulture;
+
+        var ci = new CultureInfo(culture);
+        var currentCulture = CultureInfo.CurrentCulture;
+        var currentUiCulture = CultureInfo.CurrentUICulture;
+
+        CultureInfo.CurrentCulture = ci;
+        CultureInfo.CurrentUICulture = ci;
+
+        var result = GetLocalizedString(key);
+
+        CultureInfo.CurrentCulture = currentCulture;
+        CultureInfo.CurrentUICulture = currentUiCulture;
+
+        return result!;
+    }
+
+    public LocalizedString GetDefaultLocalizedString(string key) =>
+        GetLocalizedString(key, DefaultCulture);
+}
