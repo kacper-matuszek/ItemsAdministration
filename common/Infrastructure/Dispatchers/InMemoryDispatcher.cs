d14201a10ec905a863e75397bd3f5d8205dea3c8
Kacper096 <kacpermatuszek2@gmail.com>
2023-02-03 13:39:02 +0100

    Add in memory dispatcher.

---
+++ common/Infrastructure/Dispatchers/InMemoryDispatcher.cs
@@ -0,0 +1,24 @@
+﻿using ItemsAdministration.Common.Application.Abstractions.Interfaces.Commands;
+using ItemsAdministration.Common.Application.Abstractions.Interfaces.Dispatchers;
+using ItemsAdministration.Common.Application.Abstractions.Interfaces.Queries;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ItemsAdministration.Common.Infrastructure.Dispatchers;
+
+public sealed class InMemoryDispatcher : IDispatcher
+{
+    private readonly IMediator _mediator;
+
+    public InMemoryDispatcher(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public Task Send(ICommand command, CancellationToken cancellationToken = default) =>
+        _mediator.Send(command, cancellationToken);
+
+    public Task<TResult> Query<TResult>(IQuery<TResult> query, CancellationToken cancellationToken = default) =>
+        _mediator.Send(query, cancellationToken);
+}
