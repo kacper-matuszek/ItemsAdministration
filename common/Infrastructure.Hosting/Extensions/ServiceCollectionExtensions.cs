8a3e04e608e047d75fa15f7960d6aa7293b8d2fc
Kacper096 <kacpermatuszek2@gmail.com>
2023-02-03 13:58:25 +0100

    Register command, query handlers.

--- common/Infrastructure.Hosting/Extensions/ServiceCollectionExtensions.cs
+++ common/Infrastructure.Hosting/Extensions/ServiceCollectionExtensions.cs
@@ -1,17 +1,42 @@
 ﻿using System;
 using System.Linq;
 using System.Reflection;
+using ItemsAdministration.Common.Application.Abstractions.Interfaces.Dispatchers;
 using ItemsAdministration.Common.Infrastructure.Attributes;
+using ItemsAdministration.Common.Infrastructure.Dispatchers;
 using ItemsAdministration.Common.Infrastructure.Options;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MediatR;
 
 namespace ItemsAdministration.Common.Infrastructure.Hosting.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    internal static IServiceCollection AddPostgres<TContext>(
+    public static IServiceCollection AddCqrs(this IServiceCollection services, Assembly[] assemblies)
+    {
+        const string appAssemblySuffix = "Application";
+        const string appAbstractionsAssemblySuffix = "Application.Abstractions";
+
+        var appAssemblies = assemblies.Select(a => new
+        {
+            AssemblyName = a.GetName().Name,
+            Assembly = a
+        }).Where(a => a.AssemblyName != null && (a.AssemblyName.EndsWith(appAssemblySuffix) || a.AssemblyName.EndsWith(appAbstractionsAssemblySuffix)))
+        .Select(a => a.Assembly)
+        .ToArray();
+
+        if (appAssemblies.Length == 0)
+            throw new ArgumentException("Provided assemblies are incorrects. You must provide application and application abstractions assemblies.");
+
+        services.AddSingleton<IDispatcher, InMemoryDispatcher>();
+        services.AddMediatR(appAssemblies);
+
+        return services;
+    }
+
+    public static IServiceCollection AddPostgres<TContext>(
         this IServiceCollection services, IConfiguration configuration)
         where TContext : DbContext
     {
