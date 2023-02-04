21e34d0c94403a1ea3b969fbfe7ab3fad84b3908
kacper-matuszek <kacpermatuszek2@gmail.com>
2023-02-04 15:25:16 +0100

    Add paginated list

---
+++ common/Application.Abstractions/Lists/PaginatedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ItemsAdministration.Common.Application.Abstractions.Lists;
+
+public class PaginatedList<TModel> : IReadOnlyCollection<TModel>
+{
+    public int PageSize { get; set; }
+    public int PageNumber { get; set; }
+    public int PagesCount => CountPages(PageSize, TotalItemsCount);
+    public int TotalItemsCount { get; set; }
+    public List<TModel> Elements { get; set; } = new List<TModel>();
+    public int Count => Elements.Count;
+
+    public IEnumerator<TModel> GetEnumerator() =>
+        Elements.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    private static int CountPages(int size, int itemsCount)
+        => (int)Math.Ceiling((double)itemsCount / size);
+}
