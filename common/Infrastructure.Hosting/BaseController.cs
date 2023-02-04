525ddb42d38f7cce9a147d9ced4e8d06c8a07c43
kacper-matuszek <kacpermatuszek2@gmail.com>
2023-02-04 13:53:30 +0100

    Add base controller

---
+++ common/Infrastructure.Hosting/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ItemsAdministration.Common.Application.Abstractions.Interfaces.Dispatchers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ItemsAdministration.Common.Infrastructure.Hosting;
+
+public abstract class BaseController : ControllerBase
+{
+    protected BaseController(IDispatcher dispatcher, IMapper mapper)
+    {
+        Dispatcher = dispatcher;
+        Mapper = mapper;
+    }
+
+    protected IDispatcher Dispatcher { get; }
+    protected IMapper Mapper { get; }
+}
